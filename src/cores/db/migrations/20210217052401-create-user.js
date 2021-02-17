28b81570ab07fcec6fe8bc46bac9236e982af6bc
Daniel <damledinh93.cs@gmail.com>
2021-02-17 12:40:51 +0700

    Add user model

---
+++ src/cores/db/migrations/20210217052401-create-user.js
@@ -0,0 +1,29 @@
+export default {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Users');
+  }
+};
