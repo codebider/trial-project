83b6320896f86d5987cc5a11db9613eaa5d29091
Daniel <damledinh93.cs@gmail.com>
2021-02-17 18:43:57 +0700

    Update unit test

--- __tests__/server.test.ts
+++ __tests__/server.test.ts
@@ -1,18 +1,20 @@
 import supertest from 'supertest';
+
 import newServer from '@server/server';
 
+jest.mock('@server/connectors/mysqlConnector');
 describe('Server', () => {
     let agent: any = null;
     beforeAll(() => {
         const app: any = newServer();
         agent = supertest.agent(app);
     });
-    
+
     afterAll(() => {
         agent.close();
     });
 
-    it('should pass health check',  (done) => {
+    it('should pass health check', (done) => {
         agent.get('/health').expect('"Document Management API"', done);
     });
 });
