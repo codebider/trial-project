5d707fcfa74ced541d6cef3f40d0b1364b0fa84c
Daniel <damledinh93.cs@gmail.com>
2021-02-17 09:57:28 +0700

    Setup express and typescript

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import newServer from "./server";
+import * as http from "http";
+
+const port = 3000;
+
+function startServer(): void {
+    const app = newServer();
+
+    http.createServer(app).listen(port, () => {
+        console.log(`Server ready at http://localhost:${port}`);
+    })
+}
+
+startServer();
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+function newServer(): express.Application {
+    const app = express();
+
+    app.get('/health', (_req, res) => {
+        return res.json({
+            status: 'success'
+        })
+    })
+
+    return app;
+}
+
+export default newServer;
